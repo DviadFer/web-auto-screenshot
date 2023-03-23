caf384daec5179e0f8197cfcd31125c3e1510d73
Diego Viador Ferreira <diegoviadfer@gmail.com>
2023-03-23 11:28:27 +0100

    Update index.js
    Now index.js reflects last commit tool.js changes.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const tools = require('./resources/tools');
  * It has hardcoded hints to monitor the whole process while executing it on the console.
  */
 async function run () {
-    const browser = await puppeteer.launch();
+    const project = tools.getProjectName(url[0]);
+    const browser = await puppeteer.launch({ignoreHTTPSErrors: true});
     const page = await browser.newPage();
     // Resolution folder loop
     for (const screenshotWidth of resolution) {
@@ -16,16 +17,19 @@ async function run () {
             width: screenshotWidth,
             height: 850, //If you use fullPage:true in page.screenshot(), this number is just a placeholder since it's required in page.setViewport().
         });
-        tools.manageFolders(screenshotWidth);
+        tools.manageFolders(project, screenshotWidth);
         process.stdout.write('Processing screenshots: [');
         //Screenshot file loop
         for (let [loopCount, value] of url.entries()) {
             let imageName = tools.trimUrl(value);
-            await page.goto(value, {waitUntil: 'networkidle0'});
-            await page.screenshot({path: `./images/${screenshotWidth}/${loopCount + 1}-${(imageName != "/") ? imageName : "inicio"}.png`, fullPage: true}); //Remove fullPage:true if you want static screenshots without scroll.
+            await page.goto(value, {waitUntil: 'load'});
+            
+            await tools.autoScroll(page);
+
+            await page.screenshot({path: `./images/${project}/${screenshotWidth}/${loopCount + 1}-${(loopCount != 0) ? imageName : "inicio"}.png`, fullPage: true}); //Remove fullPage:true if you want static screenshots without scroll.
             process.stdout.write(` ${loopCount + 1}`);
         }
-        process.stdout.write(` ] - ¡${screenshotWidth} resolution  completed!\n`);
+        process.stdout.write(` ] - ¡${screenshotWidth} resolution completed!\n`);
     }
     process.stdout.write('¡ALL PROCESSES COMPLETED!');
     browser.close();
